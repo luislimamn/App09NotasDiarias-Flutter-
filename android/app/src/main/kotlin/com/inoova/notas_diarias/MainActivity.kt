917f2561b0ade4f56a63cb512240cc2a04fea6e9
Luis Lima <luislimamn@gmail.com>
2024-01-10 11:42:35 -0300

    Versão 1.0.2024 SQFlite

---
+++ android/app/src/main/kotlin/com/inoova/notas_diarias/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.inoova.notas_diarias
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Home.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  final _titulo = "Notas Diarias";
+  TextEditingController _tituloController = TextEditingController();
+  TextEditingController _descricaoController = TextEditingController();
+
+  _exibirTelaCadastro() {
+    showDialog(
+      context: context,
+      builder: (context){
+        return AlertDialog(
+          title: Text("Adicionar Anotação"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              TextField(
+                controller: _tituloController,
+                autofocus: true,
+                decoration: const InputDecoration(
+                  labelText: "Título",
+                  hintText: "Digite título..."
+                ),
+              ),
+              TextField(
+                controller: _descricaoController,
+                decoration: const InputDecoration(
+                    labelText: "Descrição",
+                    hintText: "Digite a Descrição..."
+                ),
+              )
+            ],
+          ),
+          actions: <Widget>[
+            ElevatedButton(
+              style: const ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll<Color>(Colors.red),
+              ),
+              onPressed: () => Navigator.pop(context),
+              child: const Text(
+                "Cancelar",
+                style: TextStyle(
+                    color: Colors.white
+                ),
+              ),
+            ),
+            ElevatedButton(
+              style: const ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll<Color>(Colors.lightGreen),
+              ),
+              onPressed: ( ){
+                //Salvar
+                Navigator.pop(context);
+              },
+              child: const Text(
+                "Salvar",
+                style: TextStyle(
+                    color: Colors.white
+                ),
+              ),
+            )
+          ],
+        );
+      }
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_titulo),
+        backgroundColor: Colors.lightGreen,
+        foregroundColor: Colors.white,
+      ),
+      body: Container(
+
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightGreen,
+        foregroundColor: Colors.white,
+        child: const Icon(Icons.add),
+        onPressed: (){
+          _exibirTelaCadastro();
+        }
+      ),
+    );
+  }
+}
